204c71dd349eaa4097ed3cb7169dca8ad93dfa3c
saghirahmad385 <asaghir@cyangate.com>
2025-03-21 15:29:35 +0500

    Initial commit

---
+++ src/main/java/io/cyangate/daxkit/DaxkitApplication.java
@@ -0,0 +1,13 @@
+package io.cyangate.daxkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaxkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaxkitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/cyangate/daxkit/DaxkitApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cyangate.daxkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaxkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
